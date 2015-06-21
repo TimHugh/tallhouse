39dc728c780a1d18465b951f8c94716dbfe491d1
Tim Heuett <tim.heuett@gmail.com>
2015-06-21 14:35:14 -0700

    move environment setup into appropriate file

--- config.ru
+++ config.ru
@@ -1,8 +1,3 @@
-require 'bundler'
-Bundler.require
-
 require './environment.rb'
-require File.join(Application.root, 'lib', 'application.rb')
-Dir[File.join(Application.root, 'lib', '**', '*.rb')].each { |f| require f }
 
 run Application
--- environment.rb
+++ environment.rb
@@ -1,5 +1,11 @@
+require 'bundler'
+Bundler.require
+
 class Application < Sinatra::Base
   configure do
     set :root, File.absolute_path('.')
   end
 end
+
+require File.join(Application.root, 'lib', 'application.rb')
+Dir[File.join(Application.root, 'lib', '**', '*.rb')].each { |f| require f }
