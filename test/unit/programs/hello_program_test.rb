a8ddff51320d31a26a3bbeef58050439a9563e37
Tim Heuett <tim.heuett@gmail.com>
2015-06-25 17:34:11 -0700

    add test for hello program

---
+++ test/unit/programs/hello_program_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class HelloProgramTest < Test
+  def program
+    @program ||= Programs::HelloProgram.new
+  end
+
+  def test_default_response
+    refute program.respond(Body: "Hello, house!").empty?
+  end
+end
