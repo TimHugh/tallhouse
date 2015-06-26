9ffa79f6840f8d286d7ee70743ba0f15262cc034
Tim Heuett <tim.heuett@gmail.com>
2015-06-26 14:11:44 -0700

    Response swallows errors--should be returning appropriate errors text

--- test/unit/response_test.rb
+++ test/unit/response_test.rb
@@ -1,19 +1,15 @@
 require 'test_helper'
 
-class ResponseTest
+class ResponseTest < Test
   def test_normal_response
     assert_equal 'Test response', Response.new(Body: 'test')
   end
 
   def test_missing_response
-    assert_raises ProgramManager::MissingCommand do
-      Response.new
-    end
+    refute Response.new().empty?
   end
 
   def test_unknown_response
-    assert_raises ProgramManager::UnknownCommand do
-      Response.new(Body: 'unknown')
-    end
+    refute Response.new(Body: 'unknown-command').empty?
   end
 end
