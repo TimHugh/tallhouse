c1210ccd910feacd8524fb2e2f5904719833660b
Tim Heuett <tim.heuett@gmail.com>
2015-07-12 18:57:45 -0700

    use different db per environment

--- lib/services/database_service.rb
+++ lib/services/database_service.rb
@@ -3,6 +3,7 @@ module DatabaseService
 
   def open_database(name = nil)
     program = name || binding.receiver.class.to_s.split('::').last
-    Mongo::Client.new("mongodb://localhost:27017/#{program}")
+    environment = ENV["RACK_ENV"]
+    Mongo::Client.new("mongodb://localhost:27017/#{program}=#{environment}")
   end
 end
