dcc41035f287008f9741e16a3a77dd37dfd3ee15
Tim Heuett <tim.heuett@gmail.com>
2015-07-17 21:53:48 -0700

    I accidentally an =

--- lib/services/database_service.rb
+++ lib/services/database_service.rb
@@ -4,6 +4,6 @@ module DatabaseService
   def open_database(name = nil)
     program = name || binding.receiver.class.to_s.split('::').last
     environment = ENV["RACK_ENV"]
-    Mongo::Client.new("mongodb://localhost:27017/#{program}=#{environment}")
+    Mongo::Client.new("mongodb://localhost:27017/#{program}#{environment}")
   end
 end
